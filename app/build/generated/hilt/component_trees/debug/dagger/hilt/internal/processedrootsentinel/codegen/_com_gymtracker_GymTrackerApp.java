c2ff8572f8ce580de27b48d7f9372b48f4af4116
unknown <thiago_hsmorais1@hotmail.com>
2026-06-17 16:11:11 -0300

    chore: add gitignore and remove generated files

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_gymtracker_GymTrackerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.gymtracker.GymTrackerApp"
-)
-public final class _com_gymtracker_GymTrackerApp {
-}
